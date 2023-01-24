06f4d98a193b3bf64605cdfba4354b12a3f02c45
arislid <arislid@gmail.com>
2023-01-24 00:02:23 +0000

    delete: SDTA Encoder

--- custom/modeling_segformer.py
+++ custom/modeling_segformer.py
@@ -147,75 +147,7 @@ class SegformerOverlapPatchEmbeddings(nn.Module):
         return embeddings, height, width
     
     
-'''
-class SDTAEncoder(nn.Module):
-    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6, expan_ratio=4,
-                 use_pos_emb=True, num_heads=8, qkv_bias=True, attn_drop=0., drop=0., scales=1):
-        super().__init__()
-        width = max(int(math.ceil(dim / scales)), int(math.floor(dim // scales)))
-        self.width = width
-        if scales == 1:
-            self.nums = 1
-        else:
-            self.nums = scales - 1
-        convs = []
-        for i in range(self.nums):
-            convs.append(nn.Conv2d(width, width, kernel_size=3, padding=1, groups=width))
-        self.convs = nn.ModuleList(convs)
 
-        self.pos_embd = None
-        if use_pos_emb:
-            self.pos_embd = PositionalEncodingFourier(dim=dim)
-        self.norm_xca = nn.LayerNorm(dim, eps=1e-6)
-        self.gamma_xca = nn.Parameter(layer_scale_init_value * torch.ones(dim),
-                                      requires_grad=True) if layer_scale_init_value > 0 else None
-        self.xca = XCA(dim, num_heads=num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
-
-        self.norm = LayerNorm(dim, eps=1e-6)
-        self.pwconv1 = nn.Linear(dim, expan_ratio * dim)  # pointwise/1x1 convs, implemented with linear layers
-        self.act = nn.GELU()  # TODO: MobileViT is using 'swish'
-        self.pwconv2 = nn.Linear(expan_ratio * dim, dim)
-        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)),
-                                  requires_grad=True) if layer_scale_init_value > 0 else None
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-
-    def forward(self, x):
-        input = x
-
-        spx = torch.split(x, self.width, 1)
-        for i in range(self.nums):
-            if i == 0:
-                sp = spx[i]
-            else:
-                sp = sp + spx[i]
-            sp = self.convs[i](sp)
-            if i == 0:
-                out = sp
-            else:
-                out = torch.cat((out, sp), 1)
-        x = torch.cat((out, spx[self.nums]), 1)
-        # XCA
-        B, C, H, W = x.shape
-        x = x.reshape(B, C, H * W).permute(0, 2, 1)
-        if self.pos_embd:
-            pos_encoding = self.pos_embd(B, H, W).reshape(B, -1, x.shape[1]).permute(0, 2, 1)
-            x = x + pos_encoding
-        x = x + self.drop_path(self.gamma_xca * self.xca(self.norm_xca(x)))
-        x = x.reshape(B, H, W, C)
-
-        # Inverted Bottleneck
-        x = self.norm(x)
-        x = self.pwconv1(x)
-        x = self.act(x)
-        x = self.pwconv2(x)
-        if self.gamma is not None:
-            x = self.gamma * x
-        x = x.permute(0, 3, 1, 2)  # (N, H, W, C) -> (N, C, H, W)
-
-        x = input + self.drop_path(x)
-
-        return x
-'''
     
 # Customize XCA from EdgeNeXt to adjust to Segformer
 class SegformerXCA(nn.Module):
